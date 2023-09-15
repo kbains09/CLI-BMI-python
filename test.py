60079fb21f735faf25dc7a56f092f216e82d9280
Kirat Bains <kiratbains09@gmail.com>
2023-09-15 14:09:44 -0700

    feat: testing

--- test.py
+++ test.py
@@ -8,12 +8,13 @@ class TestBMICalculator(unittest.TestCase):
         self.assertEqual(interpret_bmi(23.49), "Normal weight")
 
     def test_underweight_case(self):
-        self.assertAlmostEqual(calculate_bmi(100, 6, 0), 13.61, places=2)
-        self.assertEqual(interpret_bmi(13.61), "Underweight")
+        self.assertAlmostEqual(calculate_bmi(100, 6, 0), 13.56, places=2)  # Updated expected value
+        self.assertEqual(interpret_bmi(13.56), "Underweight")
 
     def test_overweight_case(self):
-        self.assertAlmostEqual(calculate_bmi(200, 5, 5), 33.18, places=2)
-        self.assertEqual(interpret_bmi(33.18), "Overweight")
+        self.assertAlmostEqual(calculate_bmi(200, 5, 5), 33.28, places=2)  # Updated expected value
+        self.assertEqual(interpret_bmi(33.28), "Overweight")
+
 
 if __name__ == '__main__':
     unittest.main()
